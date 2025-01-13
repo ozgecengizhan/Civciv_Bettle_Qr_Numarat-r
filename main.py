2adf9b08a0f43926d06637e760e7b3217b817075
FIRST_NAME LAST_NAME <mail@adress.com>
2025-01-13 17:54:03 +0300

    qr code logo ekleme

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import qrcode
+from PIL import Image
 
 qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
 qr.add_data("tel:+90##########")
 qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white").convert("RGB")
+
+logo = Image.open("./bettle.png").resize((80, 80))
+position = ((img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
+img.paste(logo, position)
+
+
+img.save("qr_code.png")
